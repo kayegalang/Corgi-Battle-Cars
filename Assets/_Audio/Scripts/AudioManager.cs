3cc47e7864aaf1bb9666178fa6e98021d6cee796
lilyisapenguin <lvdelchiaro@gmail.com>
2026-04-16 16:31:45 -0700

    scripts added for hooking up ui slider to volume control
    im pushing this before i do the terminal stuff cause im scared ill lose it xoxo

--- Assets/_Audio/Scripts/AudioManager.cs
+++ Assets/_Audio/Scripts/AudioManager.cs
@@ -1,10 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 
 namespace _Audio.scripts 
 {
     public class AudioManager : MonoBehaviour
     {
+        [Header("Volume")] 
+        [Range(0, 1)] 
+        public float masterVolume = 1;
+        [Range(0, 1)]
+        public float musicVolume = 1;
+        [Range(0, 1)]
+        public float sfxVolume = 1;
+        [Range(0, 1)] 
+        
+        private Bus masterBus;
+        private Bus musicBus;
+        private Bus sfxBus;
+        
         public static AudioManager instance { get; private set; }
 
         private void Awake()
@@ -14,6 +29,17 @@ namespace _Audio.scripts
                 Debug.LogError("Shiver me timbers! More than one Audio Manager was found in the scene.");
             }
             instance = this;
+            
+            masterBus = RuntimeManager.GetBus("bus:/");
+            musicBus = RuntimeManager.GetBus("bus:/Music");
+            sfxBus = RuntimeManager.GetBus("bus:/SFX");
+        }
+
+        private void Update()
+        {
+            masterBus.setVolume(masterVolume);
+            musicBus.setVolume(musicVolume);
+            sfxBus.setVolume(sfxVolume);
         }
 
         public void PlayOneShot(EventReference sound, Vector3 worldPos)
